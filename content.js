d3c8d3f2629dba043e05ccebf20c70228bd8c99c
ObiArt <58375833+ObiArt@users.noreply.github.com>
2019-12-08 20:09:24 +0300

    Now it works on search page (well, sort of)

--- content.js
+++ content.js
@@ -1,15 +1,44 @@
-let images = document.getElementsByTagName('img')
+const parser = new DOMParser();
+
+images = document.getElementsByTagName('img')
+
+function getFromTextbox(textarea){
+    exploit = textarea[0].textContent
+    startingPoint = exploit.search('src="') + 5
+    endingPoint = exploit.search('" />') - 4
+
+    return exploit.slice(startingPoint, endingPoint)
+}
 
 for(let i = 0; i < images.length; i++){
+    //Home page
     if (images[i].src.search("_blur") != -1) {
         images[i].src = images[i].src.replace("manganew_thumbs_blur", "manganew_thumbs")
     }
 
+    //Search page
+    if (images[i].src.search("/tags/") != -1){
+        fetch(images[i].parentNode.href).then(r => r.text()).then(result => {
+            htmlDocument = parser.parseFromString(result, "text/html");
+            textareal = htmlDocument.getElementsByTagName('textarea')
+
+            tocheck = images[i]
+            var z = 0; //checking if this node first or second
+            while((tocheck = tocheck.previousSibling) != null) z++;
+
+            console.log(z)
+
+            if (z == 1){
+                images[i].src = getFromTextbox(textareal).replace("imgcover.", "img.").replace("manganew_thumbs_blur","manganew_thumbs") + ".jpg"
+            } else {
+                //TODO: WHAT THE FUCK IS WRONG WITH THEIR STUPID FILE NAMING?!
+            }
+        })
+    }
+
+    //Manga page
     if (images[i].id == "cover" && images[i].src.search(".html") != -1){
-        textarea = document.getElementsByTagName('textarea')
-        exploit = textarea[0].textContent
-        startingPoint = exploit.search('src="') + 5
-        endingPoint = exploit.search('" />') - 4
-        images[i].src = exploit.slice(startingPoint, endingPoint).replace("imgcover.", "img.").replace("manganew_thumbs_blur","showfull_retina/manga") + ".jpg"
+        textareal = document.getElementsByTagName('textarea')
+        images[i].src = getFromTextbox(textareal).replace("imgcover.", "img.").replace("manganew_thumbs_blur","showfull_retina/manga") + ".jpg"
     }
 }
