1a0d103cdbd3f470669d7d4f714e65d6bab49bd2
ObiArt <58375833+ObiArt@users.noreply.github.com>
2019-12-10 22:42:25 +0300

    some boring code cleaning

--- background.js
+++ background.js
@@ -3,10 +3,10 @@ const parser = new DOMParser();
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     htmlDocument = parser.parseFromString(request, "text/html")
     fetch(htmlDocument.getElementById("manga_images").getElementsByTagName("a")[0].href).then(r => r.text()).then(result => {
-        startingPoint = result.search('"thumbs":')
-        endingPoint = result.slice(startingPoint, result.length).search("]") + startingPoint + 1
-        exploit = JSON.parse("{" + result.slice(startingPoint, endingPoint).replace(/'/g, '"') + "}")
-        pages = exploit.thumbs
+        var startingPoint = result.search('"thumbs":')
+        var endingPoint = result.slice(startingPoint, result.length).search("]") + startingPoint + 1
+        var exploit = JSON.parse("{" + result.slice(startingPoint, endingPoint).replace(/'/g, '"') + "}")
+        var pages = exploit.thumbs
 
         sendResponse(pages[Math.round(pages.length/2)+1].replace("http://","https://").replace("manganew_webp_thumbs", "manganew_thumbs").replace(".webp", ".jpg"))
     })
--- content.js
+++ content.js
@@ -3,9 +3,9 @@ const parser = new DOMParser();
 images = document.getElementsByTagName('img')
 
 function getFromTextbox(textarea){
-    exploit = textarea[0].textContent
-    startingPoint = exploit.search('src="') + 5
-    endingPoint = exploit.search('" />') - 4
+    var exploit = textarea[0].textContent
+    var startingPoint = exploit.search('src="') + 5
+    var endingPoint = exploit.search('" />') - 4
 
     return exploit.slice(startingPoint, endingPoint)
 }
@@ -17,14 +17,14 @@ for(let i = 0; i < images.length; i++){
     //Search page
     if (images[i].src.search(".jpg") == -1 && images[i].src.search(".png") == -1 && images[i].src.search(".gif") == -1 && images[i].src.search(".webp") == -1){
         try{
-            wheretogo = ""
+            var wheretogo = ""
             if (images[i].parentNode.nodeName == "A") wheretogo = images[i].parentNode.parentNode.nextSibling.nextSibling.childNodes[3].childNodes[0].href
             if (images[i].parentNode.nodeName == "DIV") wheretogo = images[i].parentNode.nextSibling.nextSibling.childNodes[1].childNodes[0].href
             fetch(wheretogo).then(r => r.text()).then(result => {
-                htmlDocument = parser.parseFromString(result, "text/html")
-                textareal = htmlDocument.getElementsByTagName('textarea')
+                var htmlDocument = parser.parseFromString(result, "text/html")
+                var textareal = htmlDocument.getElementsByTagName('textarea')
 
-                tocheck = images[i]
+                var tocheck = images[i]
                 var z = 0; //checking if this node first or second
                 while((tocheck = tocheck.previousSibling) != null) z++;
 
@@ -44,7 +44,7 @@ for(let i = 0; i < images.length; i++){
 
     //Manga page
     if (images[i].id == "cover" && images[i].src.search(".html") != -1){
-        textareal = document.getElementsByTagName('textarea')
+        var textareal = document.getElementsByTagName('textarea')
         images[i].src = getFromTextbox(textareal).replace("imgcover.", "img.").replace("manganew_thumbs_blur","showfull_retina/manga") + ".jpg"
     }
 }
