58f63a770a059860e85b885544dab0125553f67e
ObiArt <58375833+ObiArt@users.noreply.github.com>
2020-01-16 19:16:03 +0300

    YAY I FINALLY FIXED H-CHAN PREVIEW!

--- background.js
+++ background.js
@@ -13,20 +13,8 @@ function gettingPage(result, z){
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request)
-    if (request[0] == null){
-        fetch(request[1]).then(r => r.text()).then(website => {
-            console.log(website)
-            sendResponse(gettingPage(website, 1).replace("/manganew_thumbs/","/showfull_retina/manga/"))
-        })
-    } else {
-        htmlDocument = parser.parseFromString(request[0], "text/html")
-        nextStation = htmlDocument.getElementById("manga_images").getElementsByTagName("a")[0].href
-        nextStation = nextStation.slice(nextStation.search("/online/"))
-        if (nextStation.search("http") == -1) nextStation = "https://h-chan.me" + nextStation
-        fetch(nextStation).then(r => r.text()).then(website => {
-            sendResponse(gettingPage(website, request[1]))
-        })
-    }
+    fetch(request[0]).then(r => r.text()).then(website => {
+        sendResponse(gettingPage(website, request[1]))
+    })
     return true
 });
--- chan.js
+++ chan.js
@@ -2,14 +2,6 @@ const parser = new DOMParser()
 
 images = document.getElementsByTagName('img')
 
-function getFromTextbox(textarea){
-    var exploit = textarea[0].textContent
-    var startingPoint = exploit.search('src="') + 5
-    var endingPoint = exploit.search('" />') - 4
-
-    return exploit.slice(startingPoint, endingPoint)
-}
-
 for(let i = 0; i < images.length; i++){
     //Home page
     images[i].src = images[i].src.replace("manganew_thumbs_blur", "manganew_thumbs")
@@ -27,24 +19,17 @@ for(let i = 0; i < images.length; i++){
     } else //Search page
     if (images[i].src.search(".jpg") == -1 && images[i].src.search(".png") == -1 && images[i].src.search(".gif") == -1 && images[i].src.search(".webp") == -1 && images[i].src.search(".php") == -1){
         try{
+            var tocheck = images[i]
+            var z = 0 //checking if this node first or second (cover or page of the middle)
+            while((tocheck = tocheck.previousSibling) != null) z++;
+
             var wheretogo = ""
             if (images[i].parentNode.nodeName == "A") wheretogo = images[i].parentNode.parentNode.nextSibling.nextSibling.childNodes[3].childNodes[0].href
             if (images[i].parentNode.nodeName == "DIV") wheretogo = images[i].parentNode.nextSibling.nextSibling.childNodes[1].childNodes[0].href
-            fetch(wheretogo).then(r => r.text()).then(result => {
-                var htmlDocument = parser.parseFromString(result, "text/html")
-                var textareal = htmlDocument.getElementsByTagName('textarea')
+            wheretogo = wheretogo.replace("https://h-chan.me/manga/", "http://exhentai-dono.me/online/") + "?development_access=true"
 
-                var tocheck = images[i]
-                var z = 0; //checking if this node first or second
-                while((tocheck = tocheck.previousSibling) != null) z++;
-
-                if (z == 1 && getFromTextbox(textareal) != ""){
-                    images[i].src = getFromTextbox(textareal).replace("imgcover.", "img.").replace("manganew_thumbs_blur","manganew_thumbs") + ".jpg"
-                } else {
-                    chrome.runtime.sendMessage([result, z], function(response) { //Hey, background script! I need your help!
-                        images[i].src = response
-                    })
-                }
+            chrome.runtime.sendMessage([wheretogo, z], function(response) { //Hey, background script! I need your help!
+                images[i].src = response
             })
         }
         catch(err){
